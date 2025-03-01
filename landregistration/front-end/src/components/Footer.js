58a4661ebe91507b25fac8bd7eb1afe895c60d7c
Sanjay <sutharsh0806@gmail.com>
2025-03-01 23:30:42 +0530

    footer.js

--- landregistration/front-end/src/components/Footer.js
+++ landregistration/front-end/src/components/Footer.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles.css'; // Ensure styles are applied
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles.css";
 
 const Footer = () => {
   return (
-    <footer className="bg-dark text-white text-center py-3">
+    <footer className="bg-dark text-white text-center py-3 w-full fixed bottom-0">
       <p>© 2025 Land Registry using Blockchain. All rights reserved.</p>
     </footer>
   );
