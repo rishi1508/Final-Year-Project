4e1e5510907d885a801e4f6a41d4e98e40b8b824
Rishi Mishra <rishimishra1508@gmail.com>
2025-04-16 17:19:03 +0530

    updated LandRegistry.json

--- landregistration/front-end/src/components/Footer.js
+++ landregistration/front-end/src/components/Footer.js
@@ -10,17 +10,6 @@ const Footer = () => {
         {/* Navigation Links */}
         <div className="row">
           <div className="col-md-4 mb-3">
-            <h5 className="border-bottom pb-2 mb-3">About Us</h5>
-            <p className="text-light">
-              Land Registry using Blockchain is a revolutionary platform that ensures secure, transparent, and tamper-proof land registration and transfers using blockchain technology.
-            </p>
-            <div className="mt-2">
-              <FaEnvelope className="me-2" />
-              rishimishra1508@gmail.com
-            </div>
-          </div>
-
-          <div className="col-md-4 mb-3">
             <h5 className="border-bottom pb-2 mb-3">Quick Links</h5>
             <ul className="list-unstyled">
               <li className="mb-2">
@@ -49,6 +38,17 @@ const Footer = () => {
                 </a>
               </li>
             </ul>
+          </div>
+
+          <div className="col-md-4 mb-3">
+            <h5 className="border-bottom pb-2 mb-3">About Us</h5>
+            <p className="text-light">
+              Land Registry using Blockchain is a revolutionary platform that ensures secure, transparent, and tamper-proof land registration and transfers using blockchain technology.
+            </p>
+            <div className="mt-2">
+              <FaEnvelope className="me-2" />
+              rishimishra1508@gmail.com
+            </div>
           </div>
 
           <div className="col-md-4 text-center">
